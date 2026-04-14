3c1f2df75960af62e6dcb46f8cbb919e7d0e7a8e
şira eroğlu <siraeroglu@hotmail.com>
2026-04-14 12:03:28 +0300

    add github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  devIndicators: false,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/aylar-plastik" : "",
+  assetPrefix: isProd ? "/aylar-plastik/" : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
